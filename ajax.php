0cf1b80de8ae998d75c4cad81d632d9db1b5bbd9
LordJZ <jouterr@gmail.com>
2009-07-13 19:23:01 +0400

    Remove trash

--- ajax.php
+++ ajax.php
@@ -70,13 +70,6 @@ switch($what)
 		$x .= '});';
 		break;
 	case 'achievement':
-	/*
-$WowheadPower.registerAchievement(42, 0, {
-	name_enus: 'Explore Eastern Kingdoms',
-	icon: 'Achievement_Zone_EasternKingdoms_01',
-	tooltip_enus: '<table><tr><td><b class="q">Explore Eastern Kingdoms</b></td></tr></table><table><tr><td><br />Explore the regions of Eastern Kingdoms.<br /><br /><span class="q">Criteria:</span><table width="100%"><tr><td class="q0" style="white-space: nowrap"><small><!--cr1277:8:760-->- Alterac Mountains<br /><!--cr1279:8:765-->- Badlands<br /><!--cr1281:8:775-->- Burning Steppes<br /><!--cr1283:8:627-->- Dun Morogh<br /><!--cr1285:8:776-->- Elwynn Forest<br /><!--cr1287:8:772-->- Hillsbrad Foothills<br /><!--cr1289:8:780-->- Redridge Mountains<br /><!--cr1291:8:774-->- Searing Gorge<br /><!--cr1293:8:781-->- Stranglethorn Vale<br /><!--cr1295:8:773-->- The Hinterlands<br /><!--cr1297:8:802-->- Westfall<br /><!--cr1550:8:859-->- Eversong Woods<br /><!--cr1796:8:868-->- Isle of Quel\'Danas</small></td><th class="q0" style="white-space: nowrap; text-align: left"><small><!--cr1278:8:761-->- Arathi Highlands<br /><!--cr1280:8:766-->- Blasted Lands<br /><!--cr1282:8:777-->- Deadwind Pass<br /><!--cr1284:8:778-->- Duskwood<br /><!--cr1286:8:771-->- Eastern Plaguelands<br /><!--cr1288:8:779-->- Loch Modan<br /><!--cr1290:8:768-->- Tirisfal Glades<br /><!--cr1292:8:769-->- Silverpine Forest<br /><!--cr1294:8:782-->- Swamp of Sorrows<br /><!--cr1296:8:770-->- Western Plaguelands<br /><!--cr1298:8:841-->- Wetlands<br /><!--cr1551:8:858-->- Ghostlands</small></th></tr></table></td></tr></table>'
-});
-	*/
 		if(!$achievement = load_cache(23, $id))
 		{
 			require_once('includes/allachievements.php');
